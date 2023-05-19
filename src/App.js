3921e529ac464b2ea91c9399aa712f879bc1d2d1
karthik-kzk <qwertykzk@gmail.com>
2023-05-19 19:29:38 +0530

    addingImages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import tooth from './images/11.png'
+import cavity from './images/cavity11.png'
+import bracket1 from './images/bracketIcon.jpeg'
+import bracket2 from './images/bracketType2Icon.jpeg'
+import {useState} from 'react'
 
 function App() {
+  const imgArray=[tooth,cavity]
+  const bracketArray=[bracket1,bracket2]
+  const [state,setState]=useState(0)
+  const [bracket,setBracket]=useState(0)
+
+  function onClick(){    
+    setState(state?0:1)
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='tooth'>
+     <img  src={imgArray[state]} style={{ width: "50px", height:"auto"  }} alt="tooth"/>
+     <img  src={bracketArray[bracket]} className="bracket" alt="tooth"/>
+     <button onClick={onClick}>change teeth</button>
+     <button onClick={()=>setBracket(bracket?0:1)}>change bracket</button>
+   </div>
   );
 }
 
